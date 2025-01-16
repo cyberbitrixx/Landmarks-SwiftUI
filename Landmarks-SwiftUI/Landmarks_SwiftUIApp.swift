6aa00ea1b8c0c8c049fdd6d5a32a8201837cc06d
cyberbitrixx <shchukinasofiya@gmail.com>
2025-01-16 03:12:35 -0800

    Initial Commit

---
+++ Landmarks-SwiftUI/Landmarks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_SwiftUIApp.swift
+//  Landmarks-SwiftUI
+//
+//  Created by sofia on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
